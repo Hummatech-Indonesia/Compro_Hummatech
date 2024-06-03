ecd86ae8d559e9ef9c8112dfc9c4ebf3e5d4a21d
nesaathussholeha <nesaathussholeha@gmail.com>
2024-06-03 10:37:09 +0700

    Update product-detail.blade.php

--- resources/views/landing/product/product-detail.blade.php
+++ resources/views/landing/product/product-detail.blade.php
@@ -69,7 +69,7 @@
 @endsection
 
 @section('content')
-<div class="uk-section in-equity-16">
+<div class="uk-section">
     <div class="uk-container uk-margin-top uk-margin-large-bottom">
         <div class="uk-grid uk-grid-large uk-flex uk-flex-middle" data-uk-grid>
             <div class="uk-width-3-5@m uk-flex uk-flex-middle uk-margin-large-bottom">
