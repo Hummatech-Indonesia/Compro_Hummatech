53c856868d3a3ccd83bd6194a1320f3eb74f7c05
Abdul Kader <abdulkader0126@gmail.com>
2024-06-04 10:26:56 +0700

    portfolio

--- resources/views/landing/portfolio.blade.php
+++ resources/views/landing/portfolio.blade.php
@@ -6,8 +6,8 @@
 @endsection
 
 @section('seo')
-<meta name="description" content="Portofolio Hummatech." />
-<meta name="title" content="Portofolio - Hummatech" />
+    <meta name="description" content="Portofolio Hummatech." />
+    <meta name="title" content="Portofolio - Hummatech" />
     <!-- ========== Breadcrumb Markup (JSON-LD) ========== -->
     <script type="application/ld+json">
 {
@@ -32,66 +32,98 @@
 @endsection
 
 @section('header')
-<div class="uk-section uk-padding-remove-vertical in-equity-breadcrumb">
-    <div class="uk-container">
-        <div class="uk-grid">
-            <div class="uk-width-1-1">
-                <ul class="uk-breadcrumb">
-                    <li href="/">Beranda</li>
-                    <li>
-                        <span>Produk</span>
-                    </li>
-                </ul>
+    <div class="uk-section uk-padding-remove-vertical in-equity-breadcrumb">
+        <div class="uk-container">
+            <div class="uk-grid">
+                <div class="uk-width-1-1">
+                    <ul class="uk-breadcrumb">
+                        <li href="/">Beranda</li>
+                        <li>
+                            <span>Produk</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('content')
 
-<div class="uk-section">
-    <div class="uk-container uk-margin-top uk-margin-large-bottom">
-        <div class="uk-grid uk-grid-large uk-flex uk-flex-middle" data-uk-grid>
-            <div class="uk-width-3-5@m uk-flex uk-flex-middle uk-margin-large-bottom">
-                <div class="in-equity-video">
-                    <img class="uk-border-rounded uk-width-1-1" src="{{ asset('assets_landing/img/blockit/in-gallery-image-2.jpg') }}" data-src="{{ asset('assets_landing/img/blockit/in-gallery-image-2.jpg') }}" alt="sample-images" width="433" height="255" data-uk-img>
-                </div>
+    <div class="uk-section">
+        <div class="uk-container uk-margin-top uk-margin-large-bottom">
+            @forelse ($portfolios as $key => $portfolio)
+                {{-- @dd($compact) --}}
+                @if ($key % 2 === 1)
+                    <div class="uk-grid uk-grid-large uk-flex uk-flex-middle" data-uk-grid>
+                        <div class="uk-width-3-5@m uk-flex uk-flex-middle uk-margin-large-bottom">
+                            <div class="in-equity-video">
+                                <img class="uk-border-rounded uk-width-1-1"
+                                    src="{{ asset('storage/' . $portfolio->image) }}"
+                                    data-src="{{ asset('storage/' . $portfolio->image) }}"
+                                    alt="sample-images" width="433" height="255" data-uk-img>
+                            </div>
+                        </div>
+                        <div class="uk-width-2-5@m uk-flex uk-flex-middle">
+                            <div>
+                                <h3 style="color: black">
+                                    <a style="color: black" href="/portfolio/{{ $portfolio->slug }}">{{ $portfolio->name }}</a>
+                                </h3>
+                                <p>  {{ $portfolio->description }}</p>
+                                <a href="/portfolio/{{ $portfolio->slug }}"
+                                    class="uk-button uk-button-secondary uk-border-rounded uk-margin-small-top uk-margin-small-right">Detail
+                                    <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
+                                </a>
+                                <a target="_blank"
+                                href="{{ $portfolio->link }}"
+                                    class="uk-button uk-button-primary uk-border-rounded uk-margin-small-top">Kunjungi
+                                    Website
+                                    <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                @else
+                    <div class="uk-grid uk-grid-large uk-flex uk-flex-middle" data-uk-grid>
+                        <div class="uk-width-2-5@m uk-flex uk-flex-middle">
+                            <div>
+                                <h3 style="color: black">
+                                    <a style="color: black" href="/portfolio/{{ $portfolio->slug }}">{{ $portfolio->name }}</a>
+                                </h3>
+                                <p>  {{ $portfolio->description }}</p>
+                                <a href="/portfolio/{{ $portfolio->slug }}"
+                                    class="uk-button uk-button-secondary uk-border-rounded uk-margin-small-top uk-margin-small-right">Detail
+                                    <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
+                                </a>
+                                <a target="_blank"
+                                href="{{ $portfolio->link }}"
+                                    class="uk-button uk-button-primary uk-border-rounded uk-margin-small-top">Kunjungi
+                                    Website
+                                    <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="uk-width-3-5@m uk-flex uk-flex-middle uk-margin-large-bottom">
+                            <div class="in-equity-video">
+                                <img class="uk-border-rounded uk-width-1-1"
+                                    src="{{ asset('storage/' . $portfolio->image) }}"
+                                    data-src="{{ asset('storage/' . $portfolio->image) }}"
+                                    alt="sample-images" width="433" height="255" data-uk-img>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            @empty
+            <div class="uk-flex uk-flex-center">
+                <img src="{{ asset('empty.png') }}" alt="" srcset="">
             </div>
-            <div class="uk-width-2-5@m uk-flex uk-flex-middle">
-                <div>
-                    <h3>Tracer Study SMKN 2 Penajam Paser Utara</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur. Tincidunt pellentesque pellentesque sed in. Sit nunc velit aliquam quis faucibus nibh nisl pellentesque. Massa natoque mattis quisque ut. Lorem ipsum dolor sit amet consectetur. Tincidunt pellentesque pellentesque sed in. Sit nunc velit aliquam quis faucibus nibh nisl pellentesque. Massa natoque mattis quisque ut Baca Selengkapnya...</p>
-                    <a href="/portfolio/detail" class="uk-button uk-button-secondary uk-border-rounded uk-margin-small-top uk-margin-small-right">Detail
-                        <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
-                    </a>
-                    <a href="#" class="uk-button uk-button-primary uk-border-rounded uk-margin-small-top">Kunjungi Website
-                        <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="uk-grid uk-grid-large uk-flex uk-flex-middle" data-uk-grid>
-            <div class="uk-width-2-5@m uk-flex uk-flex-middle">
-                <div>
-                    <h3>Tracer Study SMKN 2 Penajam Paser Utara</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur. Tincidunt pellentesque pellentesque sed in. Sit nunc velit aliquam quis faucibus nibh nisl pellentesque. Massa natoque mattis quisque ut. Lorem ipsum dolor sit amet consectetur. Tincidunt pellentesque pellentesque sed in. Sit nunc velit aliquam quis faucibus nibh nisl pellentesque. Massa natoque mattis quisque ut Baca Selengkapnya...</p>
-                    <a href="/portfolio/detail" class="uk-button uk-button-secondary uk-border-rounded uk-margin-small-top uk-margin-small-right">Detail
-                        <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
-                    </a>
-                    <a href="#" class="uk-button uk-button-primary uk-border-rounded uk-margin-small-top">Kunjungi Website
-                        <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
-                    </a>
-                </div>
-            </div>
-            <div class="uk-width-3-5@m uk-flex uk-flex-middle uk-margin-large-bottom">
-                <div class="in-equity-video">
-                    <img class="uk-border-rounded uk-width-1-1" src="{{ asset('assets_landing/img/blockit/in-gallery-image-2.jpg') }}" data-src="{{ asset('assets_landing/img/blockit/in-gallery-image-2.jpg') }}" alt="sample-images" width="433" height="255" data-uk-img>
-                </div>
-            </div>
+            <h2 class="uk-text-center">
+                No data
+            </h2>
+            @endforelse
+
         </div>
     </div>
-</div>
 
 
 
