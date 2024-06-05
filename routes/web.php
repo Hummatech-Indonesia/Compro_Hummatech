c184b1634a5fb3276761a68f11e190ea9d6a4d1c
Abdul Kader <abdulkader0126@gmail.com>
2024-06-05 10:14:03 +0700

    service

--- routes/web.php
+++ routes/web.php
@@ -228,10 +228,10 @@ Route::get('news/category/{category:slug}', [NewsController::class, 'newsCategor
 Route::get('news/{slugnews}', [NewsController::class, 'showNews'])->name('news.view');
 
 //showService
-// Route::get('services/{slugService}', [ServiceController::class, 'ShowService']);
-Route::get('services/magang-pkl', function() {
-    return view('landing.service.service-detail');
-});
+Route::get('services/{slugService}', [ServiceController::class, 'ShowService']);
+// Route::get('services/magang-pkl', function() {
+//     return view('landing.service.service-detail');
+// });
 Route::get('services/magang-pkl/training', function() {
     return view('landing.service.training-detail');
 });
