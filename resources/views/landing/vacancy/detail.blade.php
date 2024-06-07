c1df9bd04cb91d3ac35264dd39f42eaaf0a54cd6
nesaathussholeha <nesaathussholeha@gmail.com>
2024-06-07 09:03:28 +0700

    Update detail.blade.php

--- resources/views/landing/vacancy/detail.blade.php
+++ resources/views/landing/vacancy/detail.blade.php
@@ -93,8 +93,45 @@
                 </div>
             </div>
         </div>
-        <div class="uk-container uk-margin-large-top uk-margin-large-bottom">
-        </div>
+            <div class="uk-grid uk-grid-stack">
+                <div class="uk-width-expand@m uk-grid-margin uk-first-column">
+                    <aside class="uk-margin-medium-bottom">
+                        <div class="uk-card uk-card-default uk-card-body uk-border-rounded">
+                            <h5 class="uk-heading-bullet uk-text-uppercase uk-text-left">Rincian</h5>
+                            <ul class="uk-list widget-categories uk-margin-remove">
+                                <li class="uk-margin-remove">
+                                    <h6 class="uk-text-left uk-margin-remove">Gaji</h6>
+                                    <p class="uk-text-left uk-margin-remove">Rp. {{ number_format($jobVacancy->salary, 0, ',', '.') }}</p>
+                                </li>
+                            </ul>
+                            <div class="uk-width-1-1@m uk-text-center uk-margin-medium-top">
+                                <a href="{{ $jobVacancy->whatsapp }}" class="uk-button uk-button-primary uk-border-rounded" target="_blank" style="background-color:#d7ac53; color:white">
+                                    Lamar <i class="fas fa-file uk-margin-small-left"></i>
+                                </a>
+                            </div>
+
+                        </div>
+                    </aside>
+                </div>
+                <div class="uk-width-2-3@m uk-first-column">
+                    <h3 class="uk-margin-remove">{{$jobVacancy->name}}</h3>
+                    <hr>
+                    <h5>Deskripsi</h5>
+                    <p class="uk-text-lead uk-text-muted">
+                        {!! $jobVacancy->description !!}
+                    </p>
+                    <h5>Kualisifikasi</h5>
+                    <p class="uk-text-lead uk-text-muted">
+                        {!! $jobVacancy->qualification !!}
+                    </p>
+                </div>
+            </div>
+    </div>
+</div>
+
+<div class="uk-section">
+    <div class="uk-container">
+
     </div>
 </div>
 @endsection
