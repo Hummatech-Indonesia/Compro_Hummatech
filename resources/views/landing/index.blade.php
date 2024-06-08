a5988beba4227e09215713a640e502daf90913fe
nesaathussholeha <nesaathussholeha@gmail.com>
2024-06-08 19:39:28 +0700

    Update index.blade.php

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -351,7 +351,7 @@
                             @elseif($index % 3 == 1)
                                 in-card-blue
                             @else
-                                in-card-green
+                                in-card-purple
                             @endif">
                             <div class="in-icon-wrap uk-margin-bottom">
                                 <i class="fas fa-seedling fa-lg"></i>
@@ -397,7 +397,7 @@
                                     <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
                                         <div class="uk-slider-items uk-flex uk-flex-center uk-grid-collapse uk-child-width-1-6@m uk-child-width-1-2@s uk-text-center in-client-logo-6" data-uk-grid>
                                             @forelse ($mitras as $mitra)
-                                                <div class="uk-tile uk-tile-default " style="background-color: transparent">
+                                                <div class=" uk-first-column uk-flex uk-flex-center" style="background-color: transparent">
                                                     <img class="uk-margin-remove" src="{{ asset('storage/' . $mitra->image) }}" alt="{{ $mitra->name }}" width="167" height="55">
                                                 </div>
                                             @empty
